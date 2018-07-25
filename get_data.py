e7897798f6bd3d41b6f960216f33d853291fe142
53X <mirage_1997@mail.ru>
2018-07-25 19:41:01 +0530

    get_data added

--- get_data.py
+++ get_data.py
@@ -4,19 +4,30 @@ Preparing the  data from the .jsonl files
 present in the dataset.
 
 '''
-
-text = []
-labels = []
+from ast import literal_eval
 
 def get_data():
 
-	with open('/home/pranay/clickbait_data/truth.jsonl', mode ='r', encoding = 'utf-8', errors = 'ignore') as file:
+	samples = []
+	labels = []
 
-		for row in file:
-			label = row.split(' ')[-5].replace('"','').rstrip(',')
-			labels.append(1) if label == 'clickbait' else labels.append(0)
+	with open('/home/pranay/clickbait_data/truth.jsonl', mode ='r', encoding = 'utf-8', errors = 'ignore') as annotation:
+
+		for row in annotation:
+			row =  literal_eval(row)
+			labels.append(1) if row['truthClass'] == 'clickbait' else labels.append(0)
 
 	print("Clickbait samples collected : {}".format(labels.count(1)))
 	print("Non-Clickbait samples collected : {}".format(labels.count(0)))		
 
-	
+	with open('/home/pranay/clickbait_data/instances.jsonl', mode='r', encoding='utf-8', errors='ignore') as content:
+
+		for row in content:
+			text = literal_eval(row)
+			samples+=text['postText']
+
+	print("Gathered {} sample texts".format(len(samples)))		
+
+if __name__ == '__main__':
+
+	get_data()
