bc8dfb25438d66684328d1e84b7a02dbcba64c2a
53X <mirage_1997@mail.ru>
2018-07-30 03:13:33 +0530

    Model added

---
+++ model.py
@@ -0,0 +1,51 @@
+import keras
+from keras.layers import Embedding, Dense, Input, Reshape, GRU, Bidirectional,  Dropout
+from keras.models import Model 
+from embedding import preprocess, get_embedding
+from keras.optimizers import RMSprop
+
+
+
+vocabulary = len(preprocess[-1]) + 1
+optimizer = RMSprop()
+
+def clickbait_model(maxlen, embedding_type):
+
+	input_layer = Input(shape=(maxlen, ), name='input_layer')
+
+	embedding = Embedding(input_dim=vocabulary, output_dim=300, input_length=maxlen,name='embedding_layer')(input)
+	
+	reshape_for_gru = Reshape(target_shape=(maxlen,  300))(embedding)
+
+	'''
+	
+	This is the GRU layer that will be used for capturing the sequential dependencies
+	in our model both along the forward and backward directions.
+	
+	'''
+
+	my_gru = GRU(128, input_shape=(maxlen, 300),  return_sequences=False)
+
+	'''
+	This layer uses the bidirectional wrapper around our custom GRU to model
+	long term dependencies in the sequence along both forward and backward directions.
+	The merge_mode parameter indicates that the final output will be a concatenation
+	of the state-vectors along both forward and backward directions. Thus for our case
+	the output of our 'bidirectional' layer will be a single vector of 256 dimensions.
+
+	'''
+
+	bidirectional = Bidirectional(my_gru, merge_mode='concat')(reshape_for_gru)
+
+	output = Dense(1, activation='sigmoid', name='output_layer')(bidirectional)
+
+	model = Model(inputs=input_layer, outputs=output)
+
+	model.compile(optimizer, loss='binary_crossentropy',  metrics=['accuracy'])
+
+	return model
+
+
+
+
+
